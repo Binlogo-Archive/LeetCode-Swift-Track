298b252095b643a5b960e82494c02ddc57f91e1b
Binboy <binboy@live.com>
2019-10-27 08:58:05 +0800

    update solution of 88. Merge Sorted Array

--- LeetCodePlayground.playground/Pages/88_MergeSortedArray.xcplaygroundpage/Contents.swift
+++ LeetCodePlayground.playground/Pages/88_MergeSortedArray.xcplaygroundpage/Contents.swift
@@ -4,20 +4,18 @@ import Foundation
 
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var nums1Copy = nums1[0..<m]
-        var p1 = 0, p2 = 0, p = 0
-        while p1 < m && p2 < n {
-            if nums1Copy[p1] < nums2[p2] {
-                nums1[p] = nums1Copy[p1]
-                p1 += 1
+        var p1 = m - 1, p2 = n - 1, p = nums1.count - 1
+        while p1 >= 0 && p2 >= 0 {
+            if nums1[p1] > nums2[p2] {
+                nums1[p] = nums1[p1]
+                p1 -= 1
             } else {
                 nums1[p] = nums2[p2]
-                p2 += 1
+                p2 -= 1
             }
-            p += 1
+            p -= 1
         }
-        if p1 < m { nums1.replaceSubrange(p..<m+n, with: nums1Copy[p1..<m]) }
-        if p2 < n { nums1.replaceSubrange(p..<m+n, with: nums2[p2..<n]) }
+        nums1.replaceSubrange(..<(p2+1), with: nums2[..<(p2+1)])
     }
 }
 
