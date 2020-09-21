d01988907ea0e2826fe0531c058851424c189d6f
Wang Xingbin <wangxingbin@didiglobal.com>
2020-09-21 19:59:34 +0800

    Update 2_AddTwoNumbers

--- LeetCodePlayground.playground/Pages/2_AddTwoNumbers.xcplaygroundpage/Contents.swift
+++ LeetCodePlayground.playground/Pages/2_AddTwoNumbers.xcplaygroundpage/Contents.swift
@@ -4,6 +4,12 @@ import Foundation
 
 //
 // 2. 两数相加
+// 给出两个非空的链表用来表示两个非负的整数。
+// 其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
+//
+// 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+//
+// 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
 //
 // 题目链接：https://leetcode-cn.com/problems/add-two-numbers/
 // 标签：链表 数学
@@ -24,7 +30,9 @@ public class ListNode {
 
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        // 保存进位
         var carry = 0
+        // 哨兵节点
         let dummy = ListNode(0)
         var node = dummy
         var l1 = l1
@@ -33,6 +41,7 @@ class Solution {
         while l1 != nil || l2 != nil {
             let v1 = l1?.val ?? 0
             let v2 = l2?.val ?? 0
+            // 两位相加再加进位
             let sum = v1 + v2 + carry
             carry = sum / 10
             node.next = ListNode(sum % 10)
@@ -41,6 +50,7 @@ class Solution {
             if l2 != nil { l2 = l2?.next }
             node = node.next!
         }
+        // 最后一位加上必要的进位
         if carry > 0 {
             node.next = ListNode(carry)
         }
